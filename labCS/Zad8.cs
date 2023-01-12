29792c0a986f3f7512e20bbd34af663cc2f65d0c
Bartosz Piekarski <21hiddenshadow@gmail.com>
2023-01-12 16:21:49 +0100

    articulation points

--- labCS/Zad8.cs
+++ labCS/Zad8.cs
@@ -8,6 +8,7 @@ public class Zad8
     {
         _time = 0;
         var bridges = new List<Tuple<int, int>>();
+        HashSet<int> articulationPoints = new();
         var visited = new bool[n];
         var disc = new int[n];
         var low = new int[n];
@@ -17,14 +18,8 @@ public class Zad8
         {
             if (!visited[i])
             {
-                DFS(graph, i, visited, disc, low, parent, bridges);
+                DFS(graph, i, visited, disc, low, parent, bridges, articulationPoints);
             }
-        }
-
-        HashSet<int> articulationPoints = new();
-        foreach (var bridge in bridges)
-        {
-            articulationPoints.Add(bridge.Item1);
         }
 
         return (bridges, articulationPoints);
@@ -36,7 +31,8 @@ public class Zad8
         int[] disc,
         int[] low,
         int[] parent,
-        List<Tuple<int, int>> bridges)
+        List<Tuple<int, int>> bridges,
+        HashSet<int> articulationPoints)
     {
         visited[node] = true;
         disc[node] = low[node] = _time++;
@@ -46,18 +42,22 @@ public class Zad8
             if (!visited[neighbour])
             {
                 parent[neighbour] = node;
-                DFS(graph, neighbour, visited, disc, low, parent, bridges);
+                DFS(graph, neighbour, visited, disc, low, parent, bridges, articulationPoints);
                 low[node] = Math.Min(low[node], low[neighbour]);
 
                 if (low[neighbour] > disc[node])
                 {
                     bridges.Add(new Tuple<int, int>(node, neighbour));
+                    articulationPoints.Add(node);
                 }
             }
             else if (neighbour != parent[node])
             {
                 low[node] = Math.Min(low[node], disc[neighbour]);
             }
+
+            if (node != 0 && low[neighbour] >= disc[node])
+                articulationPoints.Add(node);
         }
     }
 
@@ -68,8 +68,8 @@ public class Zad8
             new() {1, 2}, //0
             new() {0, 2}, //1
             new() {0, 1, 3, 4}, //2
-            new() {2}, //3
-            new() {2} //4
+            new() {2, 4}, //3
+            new() {2, 3} //4
         };
         var (bridges, articulationPoints) = FindBridgesAndArticulationPoints(graph, 5);
 
